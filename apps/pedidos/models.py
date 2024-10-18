242c4aa990a6ce14b6ae0b4fb69243dee86a80c4
wilson_rodriguez09 <samboniwilson09@gmail.com>
2024-10-18 14:32:54 -0500

    correcion otra vez de los modelos

--- apps/pedidos/models.py
+++ apps/pedidos/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from apps.cliente.models import Cliente
+from apps.producto.models import Producto
 # Create your models here.
 
 class Pedidos(models.Model):
     
     cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null = True)
+    producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True)
     OPTION_CHOISES=[('Para Llevar', 'Para Llevar'),('Consumir','Consumir')]
     ESTADO_CHOICES=[('En espera', 'En espera'),('En preparacion','En preparacion'),('Listo','Listo')]
     option = models.CharField(max_length=50, choices= OPTION_CHOISES ,default='Para Llevar')
--- apps/producto/models.py
+++ apps/producto/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
 # Create your models here.
 
 
 class Producto(models.Model):
     
+
     nombre_producto = models.CharField(max_length=100)
     precio = models.FloatField()
     descripcion = models.CharField(max_length=100)
